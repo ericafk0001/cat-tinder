f90ce36e36e999377e90ed9e2aa95619d25610f1
ericafk0001 <dxslyofficial@gmail.com>
2024-12-15 18:55:41 -0500

    upload cat images

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOMCONTENTLoaded");
+
+  const catImg = document.getElementById("cat-img");
+  const catName = document.getElementsByClassName("cat-name");
+  const catAge = document.getElementsByClassName("cat-age");
+  const catJob = document.getElementsByClassName("cat-occupation");
 });
